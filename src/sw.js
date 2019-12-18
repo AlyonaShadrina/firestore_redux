f271bc18a87e9815f3f592039f12bf8a43bdae14
Alyona Shadrina <alyon.shadrina@gmail.com>
2019-12-18 10:42:32 +0200

    [add] try to registerRoute

--- src/sw.js
+++ src/sw.js
@@ -1,10 +1,10 @@
 workbox.core.skipWaiting()
 workbox.core.clientsClaim()
 
-// workbox.routing.registerRoute(
-//     new RegExp('.*fir-todo-8a5dc.*'),
-//     workbox.strategies.networkFirst()
-// )
+workbox.routing.registerRoute(
+    new RegExp('.*fir-todo-8a5dc.*'),
+    workbox.strategies.networkFirst()
+);
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
